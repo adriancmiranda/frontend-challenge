6394e480f9315f251906e0855270799e9aff0edf
Adrian Miranda <adriancmiranda@gmail.com>
2020-02-22 14:54:45 -0300

    feat: form validation

--- source/Form.js
+++ source/Form.js
@@ -1,4 +1,4 @@
-export function doInputFormat(value, pattern, mask) {
+export function doInputMask(value, pattern, placeholder) {
 	const strippedValue = value.replace(/[^0-9]/g, '');
 	const chars = strippedValue.split('');
 	let count = 0;
@@ -12,25 +12,37 @@ export function doInputFormat(value, pattern, mask) {
 			} else {
 				formatted += item;
 			}
-		} else if (mask) {
-			if (mask.split('')[id])
-				formatted += mask.split('')[id];
+		} else if (placeholder) {
+			if (placeholder.split('')[id])
+				formatted += placeholder.split('')[id];
 		}
 	}
 	return formatted;
 }
 
-export function inputFormatByEl(el, format, mask) {
-	const val = doInputFormat(el.value, format);
-	el.value = doInputFormat(el.value, format, mask);
+export function inputMaskByEl(el, format, placeholder) {
+	var val = doInputMask(el.value, format);
+	el.value = doInputMask(el.value, format, placeholder);
 	if (el.createTextRange) {
-		const range = el.createTextRange();
+		var range = el.createTextRange();
 		range.move('character', val.length);
 		range.select();
 	} else if (el.selectionStart) {
 		el.focus();
 		el.setSelectionRange(val.length, val.length);
 	}
+}
+
+export function inputMasksByEl(el, format, placeholder, splitChar = ', ') {
+	const formatList = format.split(splitChar);
+	const lowerFormat = formatList.reduce((a, b) => a.length <= b.length ? a : b);
+	formatList.forEach((formatItem) => {
+		if (el.value.length <= lowerFormat.length) {
+			inputMaskByEl(el, lowerFormat, placeholder);
+		} else if (formatItem.length > lowerFormat.length) {
+			inputMaskByEl(el, formatItem, placeholder);
+		}
+	});
 }
 
 export function validateFullName(value) {
@@ -48,79 +60,235 @@ export function validatePhone(value) {
 	return rule.test(value);
 }
 
+export function isString(value) {
+	return typeof value === 'string' || value instanceof String;
+}
+
+export function isFunction(value) {
+	return typeof value === 'function';
+}
+
+export function isRequired(field) {
+	return !!(field && field.required);
+}
+
+export function assignOptions(options) {
+	const opts = Object.assign({}, options);
+	opts.rules = Object.assign({}, opts.rules);
+	return opts;
+}
+
+export function assertOptions(options) {
+	if (!options) {
+		throw new Error('É necessário que você passe um objeto com opções a serem validadas');
+	}
+	if ('name' in options && !isString(options.name)) {
+		throw new Error('É necessário que o parâmetro "name" seja uma string');
+	}
+	if (options.rules === undefined || options.rules === null) {
+		throw new Error('É preciso haver um parâmetro "rules" como um objeto');
+	}
+	return options;
+}
+
+export function getParentsFromEl($el, parentsSelector) {
+	const elements = [];
+	while (($el = $el.parentElement) !== null) {
+		if ($el.nodeType !== Node.ELEMENT_NODE) {
+			continue;
+		}
+		if (!parentsSelector || $el.matches(parentsSelector)) {
+			elements.push($el);
+		}
+	}
+	return elements;
+}
+
+export function getSiblingsFromEl($el, siblingSelector) {
+	const siblings = [];
+	let sibling = $el.parentNode.firstChild;
+	while (sibling) {
+		if (sibling.nodeType === 1 && sibling !== $el) {
+			if (siblingSelector) {
+				if (sibling.matches(siblingSelector)) {
+					siblings.push(sibling);
+				}
+			} else {
+				siblings.push(sibling);
+			}
+		}
+		sibling = sibling.nextSibling;
+	}
+	return siblings;
+}
+
 export class Form {
-	constructor(name) {
-		this.name = name;
-		this.eventMaskMap = {};
-		this.eventBlurMap = {};
+
+	constructor(options) {
+		this.options = assertOptions(assignOptions(options));
+		this.form = document[this.options.name];
+		this.registerEvents = this.registerEvents.bind(this);
+		this.unregisterEvents = this.unregisterEvents.bind(this);
+		this.onChangeField = this.onChangeField.bind(this);
+		this.submit = this.submit.bind(this);
 		this.validate = this.validate.bind(this);
+		this.clearErrors = this.clearErrors.bind(this);
+		this.getErrors = this.getErrors.bind(this);
+		this.getFieldByName = this.getFieldByName.bind(this);
+		this.getButtons = this.getButtons.bind(this);
+		this.getFields = this.getFields.bind(this);
+		this.eventsMaskMap = {};
+		this.submitting = false;
 	}
 
 	registerMaskKeyUpEventByEl(el) {
-		const format = el.getAttribute('data-format');
-		const mask = el.getAttribute('data-mask');
-		if (this.eventMaskMap[el]) return;
-		this.eventMaskMap[el] = () => {
-			inputFormatByEl(el, format, mask);
+		const format = el.getAttribute('data-mask-format');
+		const placeholder = el.getAttribute('data-mask-placeholder');
+		const splitChar = el.getAttribute('data-mask-split-char');
+		this.eventsMaskMap[el.name] = () => {
+			if (splitChar) {
+				inputMasksByEl(el, format, placeholder, splitChar);
+			} else {
+				inputMaskByEl(el, format, placeholder);
+			}
 		};
-		el.addEventListener('keyup', this.eventMaskMap[el]);
-		inputFormatByEl(el, format, mask);
+		el.addEventListener('keyup', this.eventsMaskMap[el.name]);
+		inputMaskByEl(el, format, placeholder);
 	}
 
 	unregisterMaskKeyUpEventByEl(el) {
-		if (this.eventMaskMap[el]) {
-			el.removeEventListener('keyup', this.eventMaskMap[el]);
-			delete this.eventMaskMap[el];
-		}
+		el.removeEventListener('keyup', this.eventsMaskMap[el.name]);
 	}
 
 	registerInputBlurEventByEl(el) {
-		if (this.eventBlurMap[el]) return;
-		el.addEventListener('blur', this.validate);
-		this.eventBlurMap[el] = true;
+		el.addEventListener('blur', this.onChangeField);
+		el.addEventListener('change', this.onChangeField);
 	}
 
 	unregisterInputBlurEventByEl(el) {
-		el.removeEventListener('blur', this.validate);
-		delete this.eventBlurMap[el];
+		el.removeEventListener('blur', this.onChangeField);
+		el.removeEventListener('change', this.onChangeField);
+	}
+
+	registerButtonEventByEl(el) {
+		el.onclick = this.submit;
+	}
+
+	unregisterButtonEventByEl(el) {
+		delete el.onclick;
+	}
+
+	onChangeField() {
+		if (this.submitting) this.getErrors();
 	}
 
 	registerEvents() {
-		const maskEls = document.querySelectorAll('[data-mask]');
-		maskEls.forEach(this.registerMaskKeyUpEventByEl, this);
-		const inputEls = document.querySelectorAll('.Form-input input, .Form-input select');
-		inputEls.forEach(this.registerInputBlurEventByEl, this);
+		const $masks = this.getAllInputMask();
+		$masks.forEach(this.registerMaskKeyUpEventByEl, this);
+		const $fields = this.getFields();
+		$fields.forEach(this.registerInputBlurEventByEl, this);
+		const $buttons = this.getButtons();
+		$buttons.forEach(this.registerButtonEventByEl, this);
 	}
 
 	unregisterEvents() {
-		const maskEls = document.querySelectorAll('[data-mask]');
-		maskEls.forEach(this.unregisterMaskKeyUpEventByEl, this);
-		const inputEls = document.querySelectorAll('.Form-input input, .Form-input select');
-		inputEls.forEach(this.unregisterInputBlurEventByEl, this);
+		const $masks = this.getAllInputMask();
+		$masks.forEach(this.unregisterMaskKeyUpEventByEl, this);
+		const $fields = this.getFields();
+		$fields.forEach(this.unregisterInputBlurEventByEl, this);
+		const $buttons = this.getButtons();
+		$buttons.forEach(this.unregisterButtonEventByEl, this);
 	}
 
-	validate() {
-		const invalidFields = [];
-		if (!validateFullName(document.register.name.value)) {
-			invalidFields.push(document.register.name);
+	serialize() {
+		const $fields = this.getFields();
+		return $fields.reduce((data, $field) => {
+			data[$field.name] = $field.value;
+			return data;
+		}, {});
+	}
+
+	submit(evt) {
+		this.submitting = true;
+		if (this.validate()) {
+			const data = this.serialize();
+			const body = JSON.stringify(data);
+			fetch('/save-request', { method: 'post', body }).then((response) =>
+				response.json()
+			).then((response) => {
+				alert('[request has been saved]');
+				console.log('[request has been saved]', response);
+			});
+			this.submitting = false;
 		}
-		if (!validateEmail(document.register.email.value)) {
-			invalidFields.push(document.email.name);
-		}
-		if (!validatePhone(document.register.phone.value)) {
-			invalidFields.push(document.phone.name);
-		}
-		if (invalidFields.length) {
-			invalidFields[0].focus();
-			console.log('error!');
-			return false;
-		}
-		console.log('send!');
 		return false;
 	}
 
+	validate() {
+		const invalidFields = this.getErrors();
+		if (invalidFields.length) {
+			invalidFields[0].focus();
+			return false;
+		}
+		return true;
+	}
+
+	clearErrors() {
+		const $fields = this.getFields();
+		$fields.forEach(($field) => {
+			const $fieldInput = getParentsFromEl($field, '[data-ui-input-name]');
+			if ($fieldInput[0]) $fieldInput[0].classList.remove('is-error');
+			const $fieldError = getSiblingsFromEl($field, '[data-ui-input-error]');
+			if ($fieldError[0]) $fieldError[0].innerHTML = '&nbsp;';
+		});
+	}
+
+	getErrors(highlight = true) {
+		this.clearErrors();
+		const invalidFields = [];
+		const $fields = this.getFields();
+		$fields.forEach(($field) => {
+			const rule = this.options.rules[$field.name] || {};
+			const validate = isFunction(rule.validate);
+			const isValidRule = validate ? rule.validate($field.value) : !!$field.value;
+			if (isRequired($field) && !isValidRule) {
+				const $fieldInput = getParentsFromEl($field, '[data-ui-input-name]');
+				if (highlight && $fieldInput[0]) $fieldInput[0].classList.add('is-error');
+				const $fieldError = getSiblingsFromEl($field, '[data-ui-input-error]');
+				if ($fieldError[0]) $fieldError[0].innerHTML = rule.errorMessage || 'Campo inválido';
+				invalidFields.push($field);
+			}
+		});
+		return invalidFields;
+	}
+
+	getFieldByName(name) {
+		return document[this.options.name][name] || null;
+	}
+
+	getButtons() {
+		const formSelector = `form[name="${this.options.name}"]`;
+		return [...document.querySelectorAll([
+			`${formSelector} button[type="submit"]`,
+			`${formSelector} input[type="submit"]`,
+		].join(','))];
+	}
+
+	getAllInputMask() {
+		const formSelector = `form[name="${this.options.name}"]`;
+		return [...document.querySelectorAll(`${formSelector} input[data-mask-format]`)];
+	}
+
+	getFields() {
+		const formSelector = `form[name="${this.options.name}"]`;
+		return [...document.querySelectorAll([
+			`${formSelector} input[name]`,
+			`${formSelector} select[name]`,
+		].join(','))];
+	}
+
 	toString() {
-		return `[Form ${this.name}]`;
+		return `[Form ${this.options.name}]`;
 	}
 
 	inspect() {
--- source/index.js
+++ source/index.js
@@ -1,13 +1,43 @@
 import './index.css';
+import './assets/images/getninjas_b.svg';
 import { version, name } from '#/package.json';
-import Form from './Form';
+import Form, { validateFullName, validateEmail, validatePhone } from './Form';
 
-const form = new Form('moda-e-beleza/cabeleireiros');
+let form;
 
 const render = () => {
-	console.log(name, `v${version}`, 'up & running', form);
+	console.log(name, `v${version}`);
 	document.body.classList.remove('is-rendering');
+	form = new Form({
+		name: 'moda-e-beleza/cabeleireiros',
+		rules: {
+			'Qual será o serviço?': {
+				errorMessage: 'Você precisa informar o tipo de serviço',
+			},
+			'Para quando você precisa deste serviço?': {
+				errorMessage: 'Você precisa informar para quando precisa do serviço',
+			},
+			'name': {
+				validate: validateFullName,
+				errorMessage: 'Preencha seu nome completo',
+			},
+			'email': {
+				validate: validateEmail,
+				errorMessage: 'Preencha seu email corretamente, este e-mail é inválido',
+			},
+			'phone': {
+				validate: validatePhone,
+				errorMessage: 'Preencha seu telefone corretamente, este número de telefone é inválido',
+			},
+		},
+	});
 	form.registerEvents();
+};
+
+const dispose = () => {
+	if (form) {
+		form.unregisterEvents();
+	}
 };
 
 render();
@@ -15,12 +45,12 @@ render();
 if (module.hot) {
 	module.hot.accept(() => {
 		console.log('*** Accepting the updated render module! ***');
-		form.unregisterEvents();
+		dispose();
 		render();
 	});
 
 	module.hot.dispose(function() {
 		console.log('*** Disposing ***');
-		form.unregisterEvents();
+		dispose();
 	});
 }
